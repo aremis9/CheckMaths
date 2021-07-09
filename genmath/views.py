bf9490156ce594d0234ce59e785d55685cca6116
Kurt <kmsimera@gmail.com>
2021-07-09 20:35:30 +0800

    topics page frontend

--- genmath/views.py
+++ genmath/views.py
@@ -3,10 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    text = "Line 1 <br/> Line 2 <br/> Line 3"
-    return render(request, 'genmath/index.html', {
-        'text': text
-    })
+    return render(request, 'genmath/index.html')
 
 def fevaluation(request):
     return render(request, 'genmath/fevaluation.html')
